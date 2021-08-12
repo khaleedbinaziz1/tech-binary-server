e169f0fc1f1930de5cecbafdc3014be605977296
khaleedbinaziz1 <mayj4722@gmail.com>
2021-08-12 17:46:39 +0600

    payment gateway added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs-extra');
 const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
+const SSLCommerzPayment = require('sslcommerz')
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ujcbv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -124,3 +125,80 @@ client.connect(err => {
 });
 
 app.listen(process.env.PORT || port)
+
+
+
+app.use("/ssl-request", async (req, res, next) => {
+    const data = {
+        total_amount: 100,
+        currency: 'EUR',
+        tran_id: 'REF123',
+        success_url: 'https://peaceful-hamlet-50331.herokuapp.com/ssl-payment-success',
+        fail_url: 'https://peaceful-hamlet-50331.herokuapp.com/ssl-payment-fail',
+        cancel_url: 'https://peaceful-hamlet-50331.herokuapp.com/ssl-payment-cancel',
+        ipn_url: 'https://peaceful-hamlet-50331.herokuapp.com/ssl-payment-ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: 'cust@yahoo.com',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        multi_card_name: 'mastercard',
+        value_a: 'ref001_A',
+        value_b: 'ref002_B',
+        value_c: 'ref003_C',
+        value_d: 'ref004_D'
+    };
+    const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWD, false) //true for live default false for sandbox
+    sslcommer.init(data).then(data => {
+        //process the response that got from sslcommerz 
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+        console.log("data", data);
+        if (data?.GatewayPageURL) {
+            return res.status(200).redirect(data?.GatewayPageURL);
+        }
+        else {
+            return res.status(400).json({
+                message: "Ssl session was not successful"
+            })
+        }
+    });
+
+})
+
+app.post("/ssl-payment-success", async (req, res, next) => {
+    return res.status(200).json({
+        data: req.body
+    })
+});
+
+app.post("/ssl-payment-failure", async (req, res, next) => {
+    return res.status(200).json({
+        data: req.body
+    })
+});
+app.post("/ssl-payment-cancel", async (req, res, next) => {
+    return res.status(200).json({
+        data: req.body
+    })
+});
+app.post("/ssl-payment-inp", async (req, res, next) => {
+    return res.status(200).json({
+        data: req.body
+    })
+});
